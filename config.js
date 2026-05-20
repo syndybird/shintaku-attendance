832b5af769552f6198afc52d076d97ae2d0c6726
syndybird <37088736+syndybird@users.noreply.github.com>
2026-05-20 11:25:39 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// 👑 SHINTAKU WORKS システム設定一括管理ファイル
+const CONFIG = {
+  // ⚠️ GASのURLが変わった時は、ここ「だけ」を書き換えてください！
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxP7Xw2nUEUhdh8Nqtr7fOo7qz2VcJfOwy83Sh6nGv3NnfxaBpbPMUpskXy8B1wqGg0/exec",
+  
+  // 今後使うLIFF IDなどもここにまとめておくと管理が劇的にラクになります
+  LIFF_ID_SHIFT: "2010136950-3fse6ErK"
+};
